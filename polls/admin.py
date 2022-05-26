06821b3c7a9076665b6bdd91ca93bac8e625a9b3
Christine Greaney-Kelley <kelleyc416@gmail.com>
2022-05-26 15:00:17 +0000

    Work through testing and admin sections of django docs

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,21 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
 
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Date information', {'fields': ['pub_date']}),
+        (None,               {'fields': ['question_text']}),
+    ]
+    inlines = [ChoiceInline]
+
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+    list_filter = ['pub_date']
+
+admin.site.register(Question, QuestionAdmin)
+
